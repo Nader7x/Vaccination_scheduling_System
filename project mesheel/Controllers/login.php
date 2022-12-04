e5028ca5d757c5b4cabd3218ed5b4585f9d336f2
ibrahim <105638124+ibrahim1720@users.noreply.github.com>
2022-12-04 19:20:25 +0200

    Update login.php

--- project mesheel/Controllers/login.php
+++ project mesheel/Controllers/login.php
@@ -25,6 +25,39 @@ if ($_SERVER["REQUEST_METHOD"]=== "POST")
                 exit();
 
             }
+            else if($_POST["log_as"] === "user"){
+
+                session_start();
+                $_SESSION["Name"] = $user["Name"];
+                $_SESSION["City"] = $user["City"];
+                $_SESSION["Email"] = $user["Email"];
+                $_SESSION["Phone_no"] = $user["Phone_no"];
+                $_SESSION["National_id"] = $user["National_id"];
+                $_SESSION["password_hash"] = $user["password_hash"];
+                $_SESSION["ID"] = $user["ID"];
+                $_SESSION["Reservation_number"] = $user["Reservation_number"];
+                $_SESSION["First_dose_state"] = $user["First_dose_state"];
+                $_SESSION["Second_dose_state"] = $user["Second_dose_state"];
+                
+                //header("location: ../Views/index.php"); //b3d ma t login
+                exit();
+
+            }
+            else if($_POST["log_as"] === "vaccine_center"){
+
+                session_start();
+                $_SESSION["Name"] = $user["Name"];
+                $_SESSION["City"] = $user["City"];
+                $_SESSION["Email"] = $user["Email"];
+                $_SESSION["Contact_no"] = $user["Contact_no"];
+                $_SESSION["Address"] = $user["Address"];
+                $_SESSION["password_hash"] = $user["password_hash"];
+                $_SESSION["id"] = $user["id"];
+                
+                //header("location: ../Views/index.php"); //b3d ma t login
+                exit();
+
+            }
 
 
         }
@@ -33,5 +66,6 @@ if ($_SERVER["REQUEST_METHOD"]=== "POST")
     header("location: ../Views/login.html"); //lw m4 nfs el password
     exit();
 }
+
 
 ?>
